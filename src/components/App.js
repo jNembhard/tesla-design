4014be90328a0aa8f0fdd8b6c24d32a4127ffdc5
jNembhard <nembhardjl@outlook.com>
2021-07-07 21:30:17 -0400

    Removed extraneous data

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
-// import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-      {/* <Footer /> */}
     </div>
   );
 }
